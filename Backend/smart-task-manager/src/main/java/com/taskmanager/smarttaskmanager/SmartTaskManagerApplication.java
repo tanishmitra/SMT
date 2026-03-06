a885e62f591d7fb0b8b2d84d53b95cd27b23065c
tanishmitra <dbpctanish@gmail.com>
2026-03-06 18:26:45 +0530

    Spring_Initialize

---
+++ Backend/smart-task-manager/src/main/java/com/taskmanager/smarttaskmanager/SmartTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanager.smarttaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTaskManagerApplication.class, args);
+	}
+
+}
---
+++ Backend/smart-task-manager/src/test/java/com/taskmanager/smarttaskmanager/SmartTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanager.smarttaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
